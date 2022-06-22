ba31d5bb9e346d42edf68fc358e9eab93b7922c2
ThuHuyen731 <nguyenthuhuyen731@gmail.com>
2022-06-22 14:30:31 +0700

    Nested Routing

--- pages/article/[id].js
+++ pages/article/[id].js
@@ -1,5 +1,12 @@
+import { useRouter } from "next/router";
 function article() {
-  return <div>this is an article</div>;
+  const router = useRouter();
+  const { id } = router.query;
+
+  console.log("router", router);
+  console.log("id", id);
+
+  return <div>this is an article {id}</div>;
 }
 
 export default article;
