6f4025a0f8186d27874b933c2006e4709ea9b1f8
ThuHuyen731 <nguyenthuhuyen731@gmail.com>
2022-06-22 15:32:28 +0700

    Using the API Data

--- pages/article/[id].js
+++ pages/article/[id].js
@@ -1,3 +1,4 @@
+import { server } from "../../../config";
 import Link from "next/link";
 import { useRouter } from "next/router";
 function article({ article }) {
@@ -18,32 +19,60 @@ function article({ article }) {
 }
 
 export const getStaticProps = async (context) => {
-  const res = await fetch(
-    `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
-  );
+  const res = await fetch(`${server}/api/articles/${context.params.id}`);
+
   const article = await res.json();
 
   return {
-    props: { article },
+    props: {
+      article,
+    },
   };
 };
 
 export const getStaticPaths = async () => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/`);
-  const article = await res.json();
+  const res = await fetch(`${server}/api/articles`);
 
-  const ids = article.map((article) => article.id);
+  const articles = await res.json();
+
+  const ids = articles.map((article) => article.id);
   const paths = ids.map((id) => ({ params: { id: id.toString() } }));
 
   return {
-    // paths: {
-    //   params: { id: "1", id: "2" },
-    // },
     paths,
     fallback: false,
   };
 };
 
+// c2
+// export const getStaticProps = async (context) => {
+//   const res = await fetch(
+//     `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+//   );
+//   const article = await res.json();
+
+//   return {
+//     props: { article },
+//   };
+// };
+
+// export const getStaticPaths = async () => {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts/`);
+//   const article = await res.json();
+
+//   const ids = article.map((article) => article.id);
+//   const paths = ids.map((id) => ({ params: { id: id.toString() } }));
+
+//   return {
+//     // paths: {
+//     //   params: { id: "1", id: "2" },
+//     // },
+//     paths,
+//     fallback: false,
+//   };
+// };
+
+// c1
 // export const getServerSideProps = async (context) => {
 //   const res = await fetch(
 //     `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
