8709dc100df101ee2859f244b2afdaef5cdc882b
ThuHuyen731 <nguyenthuhuyen731@gmail.com>
2022-06-22 14:47:19 +0700

    getServerSideProps()

--- pages/article/[id].js
+++ pages/article/[id].js
@@ -1,12 +1,31 @@
+import Link from "next/link";
 import { useRouter } from "next/router";
-function article() {
-  const router = useRouter();
-  const { id } = router.query;
+function article({ article }) {
+  // const router = useRouter();
+  // const { id } = router.query;
 
-  console.log("router", router);
-  console.log("id", id);
+  // console.log("router", router);
+  // console.log("id", id);
 
-  return <div>this is an article {id}</div>;
+  return (
+    <>
+      <h1>{article.title}</h1>
+      <p>{article.body}</p>
+      <br />
+      <Link href="/">Go Back</Link>
+    </>
+  );
 }
+
+export const getServerSideProps = async (context) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+  );
+  const article = await res.json();
+
+  return {
+    props: { article },
+  };
+};
 
 export default article;
